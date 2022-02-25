dc139697b2fbe82050cb54e0bef5b12f5317680a
dirtyhawk <dirtyhawk@users.noreply.github.com>
2022-02-25 15:50:26 +0100

    code style; and fix for events without associated photos

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-I want to re-organize my personal photo	 collection to be ordered by
+# Export files from Shotwell DB to folders named <YYYY>-<MM>_<EVENT>
+
+I want to re-organize my personal photo	collection to be ordered by
 events, with the (approximate) monthly date added to the directory
 name:
 
@@ -9,15 +11,14 @@ name:
 
 I use [Shotwell](https://wiki.gnome.org/Apps/Shotwell) to organize the
 photos itself into events, then use this script to copy the then
-sorted files to a output directory with the desired subdirectoryt
+sorted files to a output directory with the desired subdirectory
 structure.
 
 The script is forked from a [similar script](https://github.com/steprobe/Shotwell-Export-To-Events) by [Stephen Rogers](https://github.com/steprobe),
 with the only addition that I added integration for the monthly string
 in my version. All credits for the idea and implementaiton go to him,
-obviously. 
+obviously.
 
 Example:
 
     sw_export_to_event.py -i ~/.local/share/shotwelll/data/photo.db -d <output_directory>
-
--- sw_export_to_event.py
+++ sw_export_to_event.py
@@ -9,21 +9,27 @@ from datetime import datetime
 from subprocess import Popen, PIPE
 from hashlib import sha1
 
-input_db=None
-dest=None
-comp_sha=True
-rotate=False
+input_db = None
+dest = None
+comp_sha = True
+rotate = False
+
 
 def show_usage():
-    print '''
-Usage:''', sys.argv[0], '''
+    print(
+        """
+Usage:""",
+        sys.argv[0],
+        """
     -i      Input Shotwell Database (required)
     -d      Destination to copy events (required)
-    -r      Rotate source photo's in place. Off by default. Requires 
+    -r      Rotate source photo's in place. Off by default. Requires
             exiftran to be installed. Will slow down process.
     -f      Compare by filename rather than sha1 of header (sha1 is slower + default)
     -h      Show this help
-'''
+""",
+    )
+
 
 def parse_args():
     global input_db
@@ -31,16 +37,16 @@ def parse_args():
     global rotate
     global comp_sha
 
-    input_db_set=False
-    dest_set=False
+    input_db_set = False
+    dest_set = False
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "i:d:rfh")
-    except getopt.GetoptError, err:
+    except getopt.GetoptError:
         show_usage()
         sys.exit(10)
 
-    for o,a in opts:
+    for o, a in opts:
         if o == "-i":
             input_db_set = True
             input_db = a
@@ -56,78 +62,106 @@ def parse_args():
             rotate = True
 
     if not input_db_set or not dest_set:
-        print "Missing options"
+        print("Missing options")
         show_usage()
         exit(10)
+
 
 def shafile(filename):
     try:
         with open(filename, "rb") as f:
             return sha1(f.read()).hexdigest()
-#Could just sha header...
-#return sha1(f.read(512)).hexdigest()
+    # Could just sha header...
+    # return sha1(f.read(512)).hexdigest()
     except IOError:
         return 0
+
 
 def main():
 
     parse_args()
 
-    print "Going to output to", dest
-    print "Going to Read database from", input_db
-    print "Rotation of source photos? ", rotate
-    print "Compare files by sha1? ", comp_sha
+    print("Going to output to", dest)
+    print("Going to Read database from", input_db)
+    print("Rotation of source photos? ", rotate)
+    print("Compare files by sha1? ", comp_sha)
 
     conn = sqlite3.connect(input_db)
     crs = conn.cursor()
-    crs.execute('select id, name from EventTable')
+    crs.execute("select id, name from EventTable")
     for row in crs:
-        if row[1] != None:
+        if row[1] is not None:
 
-            print "======================"
-            print "Dealing with Event", row[1]
+            print("======================")
+            print("Dealing with Event", row[1])
             timestamp_crs = conn.cursor()
-            timestamp_crs.execute('select exposure_time from PhotoTable where event_id=? order by timestamp limit 1', (row[0],))
-            event_ym = datetime.fromtimestamp(timestamp_crs.fetchall()[0][0]).strftime("%Y-%m")
-            print "Identified event date", event_ym
-            event_dir = dest + os.sep + event_ym + '_' + row[1].replace(os.sep, "_") #in case of seps in the string
+            timestamp_crs.execute(
+                "select exposure_time from PhotoTable where event_id=? order by exposure_time limit 1",
+                (row[0],),
+            )
+            try:
+                event_ym = datetime.fromtimestamp(timestamp_crs.fetchall()[0][0]).strftime(
+                    "%Y-%m"
+                )
+                print("Identified event date", event_ym)
+            except IndexError:
+                print("Seems no files present in DB that match the event")
+                continue
+            event_dir = (
+                dest + os.sep + event_ym + "_" + row[1].replace(os.sep, "_")
+            )  # in case of seps in the string
             if not os.path.exists(event_dir):
-                print "Creating", event_dir
+                print("Creating", event_dir)
                 os.makedirs(event_dir)
             photo_crs = conn.cursor()
-            photo_crs.execute('select filename from PhotoTable where event_id=?', (row[0],))
+            photo_crs.execute(
+                "select filename from PhotoTable where event_id=?", (row[0],)
+            )
             copy_cnt = 0
             exist_cnt = 0
             for photo in photo_crs:
 
                 if rotate:
-                    Popen(("exiftran", "-a", "-i", "-p", photo[0]), stdout=PIPE, stderr=PIPE).communicate()
+                    Popen(
+                        ("exiftran", "-a", "-i", "-p", photo[0]),
+                        stdout=PIPE,
+                        stderr=PIPE,
+                    ).communicate()
 
                 filename = event_dir + os.sep + os.path.basename(photo[0])
 
                 copy_needed = False
                 if comp_sha:
-                    copy_needed = not os.path.exists(filename) or shafile(filename) != shafile(photo[0])
+                    copy_needed = not os.path.exists(filename) or shafile(
+                        filename
+                    ) != shafile(photo[0])
                 else:
                     copy_needed = not os.path.exists(filename)
 
                 if copy_needed:
-                    sys.stdout.write('.')
+                    sys.stdout.write(".")
                     sys.stdout.flush()
 
                     shutil.copyfile(photo[0], filename)
                     copy_cnt = copy_cnt + 1
                 else:
-                    sys.stdout.write('e')
+                    sys.stdout.write("e")
                     sys.stdout.flush()
                     exist_cnt = exist_cnt + 1
 
-            print '\nCopied', copy_cnt, 'files. (Ignored', exist_cnt,'files as they already existed)'
+            print(
+                "\nCopied",
+                copy_cnt,
+                "files. (Ignored",
+                exist_cnt,
+                "files as they already existed)",
+            )
 
-    #Finally, delete empty dirs to tidy up (caused by old events in DB)
+    # Finally, delete empty dirs to tidy up (caused by old events in DB)
     for name in os.listdir(dest):
         if os.path.isdir(name) and len(os.listdir(name)) == 0:
             os.rmdir(name)
+
 
 if __name__ == "__main__":
     main()
